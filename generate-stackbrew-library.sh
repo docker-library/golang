1b9855687933a83e1b57edab76da1d650b5e8a68
Tianon Gravi <admwiggin@gmail.com>
2014-08-04 19:28:30 -0600

    Add script to generate stackbrew library file

---
+++ generate-stackbrew-library.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+declare -A aliases
+aliases=(
+	[1.3]='1 latest'
+	[1.2.2]='1.2'
+)
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( */ )
+versions=( "${versions[@]%/}" )
+url='git://github.com/docker-library/golang'
+
+echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+
+for version in "${versions[@]}"; do
+	commit="$(git log -1 --format='format:%H' "$version")"
+	versionAliases=( $version ${aliases[$version]} )
+	
+	echo
+	for va in "${versionAliases[@]}"; do
+		echo "$va: ${url}@${commit} $version"
+	done
+	
+	for variant in onbuild cross; do
+		echo
+		for va in "${versionAliases[@]}"; do
+			if [ "$va" = 'latest' ]; then
+				va="$variant"
+			else
+				va="$va-$variant"
+			fi
+			echo "$va: ${url}@${commit} $version/$variant"
+		done
+	done
+done
