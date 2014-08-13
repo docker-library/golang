3408ad5d8b62f7e7d3a179f6f5381a6a0c5924e8
Tianon Gravi <admwiggin@gmail.com>
2014-08-13 09:17:24 -0600

    Add simple update.sh script for easy version bumps

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+for version in "${versions[@]}"; do
+	fullVersion="$(curl -sSL 'http://golang.org/dl' | grep '">go'"$version"'.*\.src.tar.gz<' | sed -r 's!.*go([^"/<]+)\.src\.tar\.gz.*!\1!' | sort -V | tail -1)"
+	(
+		set -x
+		sed -ri 's/^(ENV GOLANG_VERSION) .*/\1 '"$fullVersion"'/' "$version/Dockerfile"
+		sed -ri 's/^(FROM golang):.*/\1:'"$fullVersion"'/' "$version/"*"/Dockerfile"
+	)
+done
