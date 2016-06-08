089b335ff58fb804283e43983740f6b33b03e65e
Tianon Gravi <admwiggin@gmail.com>
2016-06-08 13:25:10 -0700

    Simplify update.sh a bit now that we no longer support 1.4 (and no longer need GOLANG_BOOTSTRAP_*)

--- update.sh
+++ update.sh
@@ -13,19 +13,11 @@ versions=( "${versions[@]%/}" )
 travisEnv=
 googleSource="$(curl -fsSL 'https://golang.org/dl/')"
 for version in "${versions[@]}"; do
-	# This is kinda gross, but 1.5+ versions install from the binary package
-	# while 1.4 installs from src
-	if [ "$version" = '1.4' ]; then
-		package='src'
-	else
-		package='linux-amd64'
-	fi
-
 	# First check for full version from GitHub as a canonical source
 	fullVersion="$(curl -fsSL "https://raw.githubusercontent.com/golang/go/release-branch.go$version/VERSION" 2>/dev/null || true)"
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find version from GitHub for $version, scraping golang download page"
-		fullVersion="$(echo $googleSource | grep -Po '">go'"$version"'.*?\.'"$package"'\.tar\.gz</a>' | sed -r 's!.*go([^"/<]+)\.'"$package"'\.tar\.gz.*!\1!' | sort -V | tail -1)"
+		fullVersion="$(echo $googleSource | grep -Po '">go'"$version"'.*?\.src\.tar\.gz</a>' | sed -r 's!.*go([^"/<]+)\.src\.tar\.gz.*!\1!' | sort -V | tail -1)"
 	fi
 	if [ -z "$fullVersion" ]; then
 		echo >&2 "warning: cannot find full version for $version"
@@ -35,45 +27,29 @@ for version in "${versions[@]}"; do
 	versionTag="$fullVersion"
 
 	# Try and fetch the checksum from the golang source page
-	sha256="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.'"$package"'\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{64})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
-	sha1="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.'"$package"'\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{40})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
-	if [ -z "$sha1" -a -z "$sha256" ]; then
+	linuxSha256="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.linux-amd64\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{64})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
+	linuxSha1="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.linux-amd64\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{40})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
+	if [ -z "$linuxSha1" -a -z "$linuxSha256" ]; then
 		echo >&2 "warning: cannot find sha256 or sha1 for $fullVersion"
 		continue
 	fi
 
-	if [ "$package" = 'src' ]; then
-		srcSha256="$sha256"
-		srcSha1="$sha1"
-	else
-		srcSha256="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.src\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{64})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
-		srcSha1="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.src\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{40})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
-	fi
+	srcSha256="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.src\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{64})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
+	srcSha1="$(echo $googleSource | grep -Po '">go'"$fullVersion"'\.src\.tar\.gz</a>.*?>[a-f0-9]{40,64}<' | sed -r 's!.*>([a-f0-9]{40})<.*!\1!; s!.*[<>]+.*!!' | tail -1)"
 
 	[[ "$versionTag" == *.*[^0-9]* ]] || versionTag+='.0'
 	(
 		set -x
 		sed -ri '
 			s/^(ENV GOLANG_VERSION) .*/\1 '"$fullVersion"'/;
-			s/^(ENV GOLANG_DOWNLOAD_SHA256) .*/\1 '"$sha256"'/;
-			s/^(ENV GOLANG_DOWNLOAD_SHA1) .*/\1 '"$sha1"'/;
+			s/^(ENV GOLANG_DOWNLOAD_SHA256) .*/\1 '"$linuxSha256"'/;
+			s/^(ENV GOLANG_DOWNLOAD_SHA1) .*/\1 '"$linuxSha1"'/;
 			s/^(ENV GOLANG_SRC_SHA256) .*/\1 '"$srcSha256"'/;
 			s/^(ENV GOLANG_SRC_SHA1) .*/\1 '"$srcSha1"'/;
 			s/^(FROM golang):.*/\1:'"$version"'/;
 		' "$version/Dockerfile" "$version/"*"/Dockerfile"
 		cp go-wrapper "$version/"
 	)
-	if [ "$version" = '1.4' ]; then
-		# 1.4 is our "bootstrap" version for all future versions
-		(
-			set -x
-			sed -ri '
-				s/^(ENV GOLANG_BOOTSTRAP_VERSION) .*/\1 '"$fullVersion"'/;
-				s/^(ENV GOLANG_BOOTSTRAP_SHA256) .*/\1 '"$srcSha256"'/;
-				s/^(ENV GOLANG_BOOTSTRAP_SHA1) .*/\1 '"$srcSha1"'/;
-			' */Dockerfile */*/Dockerfile
-		)
-	fi
 	for variant in alpine wheezy; do
 		if [ -d "$version/$variant" ]; then
 			if [ "$variant" != 'alpine' ]; then
