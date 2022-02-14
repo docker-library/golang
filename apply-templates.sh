b45e29c19cb2710d2ba76230398d878736f39db4
Tianon Gravi <admwiggin@gmail.com>
2022-02-14 11:17:05 -0800

    Add useful URL in apply-template.sh for jq-template.awk

--- apply-templates.sh
+++ apply-templates.sh
@@ -7,6 +7,7 @@ jqt='.jq-template.awk'
 if [ -n "${BASHBREW_SCRIPTS:-}" ]; then
 	jqt="$BASHBREW_SCRIPTS/jq-template.awk"
 elif [ "$BASH_SOURCE" -nt "$jqt" ]; then
+	# https://github.com/docker-library/bashbrew/blob/master/scripts/jq-template.awk
 	wget -qO "$jqt" 'https://github.com/docker-library/bashbrew/raw/1da7341a79651d28fbcc3d14b9176593c4231942/scripts/jq-template.awk'
 fi
 
